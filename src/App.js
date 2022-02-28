e7d14c42f03ea749462c3482fef14546c7338e55
Tomas Brambora <tomas@canva.com>
2022-03-01 09:35:29 +1100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [scale, setScale] = React.useState(0.02);
+  const [size, setSize] = React.useState(1000);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layer-container">
+        {new Array(1).fill(0).map((_, i) => (
+          <div
+            className="layer"
+            style={{
+              transform: `scale(${scale}) translateX(${i * 100}px)`,
+              width: `${size}px`,
+              height: `${size}px`,
+            }}
+          ></div>
+        ))}
+        <div className="legend">
+          <div>size: {Math.round(size)}px</div>
+          <div>scale: {Math.round(scale * 1000) / 1000}</div>
+        </div>
+      </div>
+      <div className="buttons-container">
+        <div style={{ display: "flex" }} className="buttons">
+          <button onClick={() => setScale((scale) => scale * 1.1)}>
+            scale +
+          </button>
+          <button onClick={() => setScale((scale) => scale * 0.9)}>
+            scale -
+          </button>
+        </div>
+        <div style={{ display: "flex" }} className="buttons">
+          <button onClick={() => setSize((size) => size * 1.5)}>size +</button>
+          <button onClick={() => setSize((size) => size * 0.5)}>size -</button>
+        </div>
+      </div>
     </div>
   );
 }
